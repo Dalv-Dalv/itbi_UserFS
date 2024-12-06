efc8b401adb8dcd3ef8f1b92e2feda5882c03f86
Ghita Vladut <futurevlad2@gmail.com>
2024-12-06 13:21:05 +0200

    merge test

--- userFS.sh
+++ userFS.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Lista de utilizatori si procesele lor"
+
 ps -e -o user,command | awk '{print $1,$2}' | while read user pid; do
     echo "Utilizator: $user Comanda: $pid"
 done
+
+echo "SFARSIT LISTA"
