76f62c25f42e502dc25ea042a908a2edbf1f9896
PopaBogdan498 <bogdan-constantin.popa1@s.unibuc.ro>
2024-12-06 13:14:52 +0200

    admin

--- userFS.sh
+++ userFS.sh
@@ -4,6 +4,10 @@ echo "Lista de utilizatori si procesele lor"
 
 ps -e -o user,command | awk '{print $1,$2}' | while read user pid; do
     echo "Utilizator: $user Comanda: $pid"
+    if [ "$user" = "root"]; then
+    echo "ADMIN USER"
+    fi
 done
+
 
 echo "SFARSIT LISTA"
