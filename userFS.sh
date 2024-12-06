900e0f7643d08dd6d75913b63b41ee31697e4e33
PopaBogdan498 <bogdan-constantin.popa1@s.unibuc.ro>
2024-12-06 13:14:52 +0200

    admin

--- userFS.sh
+++ userFS.sh
@@ -2,4 +2,7 @@
 
 ps -e -o user,command | awk '{print $1,$2}' | while read user pid; do
     echo "Utilizator: $user Comanda: $pid"
+    if [ "$user" = "root"]; then
+    echo "ADMIN USER"
+    fi
 done
