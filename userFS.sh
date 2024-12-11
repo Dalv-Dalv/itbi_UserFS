93e1849b6aea73d882c8b2f39450a628a99477f8
Ghita Vladut <futurevlad2@gmail.com>
2024-12-11 12:31:51 +0200

    created procs

--- userFS.sh
+++ userFS.sh
@@ -2,11 +2,16 @@
 
 echo "Lista de utilizatori si procesele lor"
 
-ps -e -o user,command | awk '{print $1,$2}' | while read user pid; do
-    echo "Utilizator: $user Comanda: $pid"
-    if [ "$user" = "root"]; then
-    echo "ADMIN USER"
-    fi
+mkdir -p ./radacina 
+
+ps -e -o user,command | awk '{print $1,$2}' | while read user cmd; do
+    echo "Utilizator: $user Comanda: $cmd"
+
+    dir=./radacina/$user
+
+    mkdir -p $dir
+
+    echo $cmd >> $dir/procs
 done
 
 
